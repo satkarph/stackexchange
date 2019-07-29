fb0cd394307f261d610be8bca3491c9d7cf7b3de
satkarph <satkarphuyal@hotmail.com>
2019-07-29 21:21:07 +0545

    adding static

--- stackexchange/settings.py
+++ stackexchange/settings.py
@@ -135,7 +135,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 DEFAULT_EMAIL_FROM = 'info@genesesofts.com'
 DEFAULT_EMAIL_BCC = ''
